b10bb8eb214dc86254b14895f405e98c6170ef17
memphis-tools <rn@github.com>
2023-07-07 22:39:04 +0200

    13-vue Posts, ne pas afficher les boutons de parcours si aucuns posts

--- litreview/views.py
+++ litreview/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.core.exceptions import ObjectDoesNotExist
 from itertools import chain
 
 from authentication.models import User, UserFollows
